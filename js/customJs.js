377e44e78b37cb45ee4ff391ec189f187ac10db1
Brad <brad.raymond.griffin@gmail.com>
2018-10-04 19:51:41 -0400

    revert

--- js/customJs.js
+++ js/customJs.js
@@ -389,7 +389,7 @@ function fitnessfunction(a){
 }
 
 function rotationalDifference(rotationObj){
-      var retVal = getRandomInt(1,3);
+      var retVal = 1;
       var wastedSpace = 0;
       switch (retVal){
             case 1:
