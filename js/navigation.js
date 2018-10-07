9dc3f9009e6cbab767cd906deb5faf19518e41df
Brad <brad.raymond.griffin@gmail.com>
2018-10-07 19:48:05 -0400

    changes

--- js/navigation.js
+++ js/navigation.js
@@ -3,26 +3,22 @@
 $(function(){
 
 
-
+//Loads header libraries
       if ($("#linkContent").length){
             var linkContent = "<meta charset=\"utf-8\"><title>Brad Griffin, Professional</title><meta content=\"IE=edge\" http-equiv=\"X-UA-Compatible\"><meta content=\"width=device-width, initial-scale=1\" name=\"viewport\"><meta content=\"Brad Griffin, Technology Professional Portfolio\" name=\"description\"><meta content=\"Brad Griffin\" name=\"author\"><meta name=\"keywords\" content = \"technology, professional, student, college, intern, professionals, thomas, thomas college\"><link href=\"images/logo.png\" rel=\"shortcut icon\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\"><link href=\"https://fonts.googleapis.com/css?family=Roboto+Slab:400,700\" rel=\"stylesheet\" type=\"text/css\"><link href=\"https://fonts.googleapis.com/css?family=Roboto:400,300,500,700\" rel=\"stylesheet\" type=\"text/css\"><link href=\"css/bootstrap.min.css\" rel=\"stylesheet\"><link href=\"css/owl.carousel.css\" rel=\"stylesheet\"><link href=\"css/bootstrap.min.css\" rel=\"stylesheet\"><link href=\"css/owl.carousel.css\" rel=\"stylesheet\"><link href=\"css/owl.theme.css\" rel=\"stylesheet\"><link href=\"css/magnific-popup.css\" rel=\"stylesheet\"><link href=\"css/style.css\" rel=\"stylesheet\"><link href=\"css/responsive.css\" rel=\"stylesheet\"><script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script><script type=\"text/javascript\" src=\"js/navigation.js\"></script>";
 
             $("#linkContent").html(linkContent);
       }
 
-      if( $('#splash-screen').length )
-      {
-            var splashScreen = "<section id=\"home\"><div class=\"container\"><div class=\"row\"><div class=\"wrap-hero-content\"><div class=\"hero-content\"><h1>Hello</h1><br><span class=\"typed\"></span></div></div><div class=\"mouse-icon margin-20\"><div class=\"scroll\"></div></div></div></div></section>";
-          $("#splash-screen").html(splashScreen);
-      }
-
+//Loads footer libraries
       if($("#innerJavascript").length){
             var innerJavascript = "<script src=\"js/jquery-1.9.1.min.js\" type=\"text/javascript\"></script><script src=\"js/jquery.appear.js\" type=\"text/javascript\"></script><script src=\"js/bootstrap.min.js\" type=\"text/javascript\"></script><script src=\"js/classie.js\" type=\"text/javascript\"></script><script src=\"js/owl.carousel.min.js\" type=\"text/javascript\"></script><script src=\"js/jquery.magnific-popup.min.js\" type=\"text/javascript\"></script><script src=\"js/masonry.pkgd.min.js\" type=\"text/javascript\"></script><script src=\"js/masonry.js\" type=\"text/javascript\"></script><script src=\"js/smooth-scroll.min.js\" type=\"text/javascript\"></script><script src=\"js/typed.js\" type=\"text/javascript\"></script><script src=\"js/main.js\" type=\"text/javascript\"></script>";
             $("#innerJavascript").html(innerJavascript);
       }
 
+//Loads navigation
       if($("#navigationContent").length){
-            var navigationContainer = "<!--PRELOADER--><div class=\"header\"><div class=\"for-sticky\"><!--LOGO--><div class=\"col-md-2 col-xs-6 logo\"><a href=\"projects.html\" style = \"font-size: 30px;\"><img class=\"logo-nav\"><b style = \"font-size:35px;\">B</b>rad</a></div><!--/.LOGO END--></div><div class=\"menu-wrap\"><nav class=\"menu\"><div class=\"menu-list\"><a data-scroll=\"\" href=\"#home\" class=\"active\"><span>Home</span></a><a data-scroll=\"\" href=\"#container-content\" onclick=\"loadTemplate()\"><span>About</span></a><a data-scroll=\"\" href=\"#work\"><span>Work</span></a><a data-scroll=\"\" href=\"#skill\"><span>Skills</span></a><a data-scroll=\"\" href=\"#education\"><span>Education</span></a><a data-scroll=\"\" href=\"#contact\"><span>Contact</span></a><a href=\"https://bradleyrgriffin.github.io/geneticAlgorithm\"><span>Genetic Algorithm</span></a></div></nav><button class=\"close-button\" id=\"close-button\" onclick=\"hideMenu()\">Close Menu</button></div><button class=\"menu-button\" id=\"open-button\" onclick=\"showMenu()\"><span></span><span></span><span></span></button><!--/.for-sticky--></div>";
+            var navigationContainer = "<!--PRELOADER--><div class=\"header\"><div class=\"for-sticky\"><!--LOGO--><div class=\"col-md-2 col-xs-6 logo\"><a href=\"projects.html\" style = \"font-size: 30px;\"><img class=\"logo-nav\"><b style = \"font-size:35px;\">B</b>rad</a></div><!--/.LOGO END--></div><div class=\"menu-wrap\"><nav class=\"menu\"><div class=\"menu-list\"><a data-scroll=\"\"  href=\"https://bradleyrgriffin.me/\" class=\"active\"><span>Home</span></a><a data-scroll=\"\" onclick=\"loadTemplate()\"><span>Genetic Algorithm</span></a><a data-scroll=\"\" onclick=\"loadTemplate()\" ><span>3D Graphics</span></a><a data-scroll=\"\" onclick=\"loadTemplate()\"><span>2D Graphics</span></a></div></nav><button class=\"close-button\" id=\"close-button\" onclick=\"hideMenu()\">Close Menu</button></div><button class=\"menu-button\" id=\"open-button\" onclick=\"showMenu()\"><span></span><span></span><span></span></button><!--/.for-sticky--></div>";
             $("#navigationContent").html(navigationContainer);
       }
 
@@ -31,7 +27,7 @@ $(function(){
 });
 
 function loadTemplate(){
-      $('#container-content').load("blog/mainpage.html");
+      $('#container-content').load("blog/createawebsite.html");
 }
 
 function showMenu(){
