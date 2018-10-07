2240c0b6bfa9bd0ace25a566e6513342548d7c6c
Brad <brad.raymond.griffin@gmail.com>
2018-10-07 19:18:12 -0400

    Update js/navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -2,11 +2,7 @@
 
 $(function(){
 
-      if( $('#splash-screen').length )
-      {
-            var splashScreen = "<section id=\"home\"><div class=\"container\"><div class=\"row\"><div class=\"wrap-hero-content\"><div class=\"hero-content\"><h1>Hello</h1><br><span class=\"typed\"></span></div></div><div class=\"mouse-icon margin-20\"><div class=\"scroll\"></div></div></div></div></section>";
-          $('#splash-screen').html(splashScreen);
-      }
+
 
       if ($("#linkContent").length){
             var linkContent = "<meta charset=\"utf-8\"><title>Brad Griffin, Professional</title><meta content=\"IE=edge\" http-equiv=\"X-UA-Compatible\"><meta content=\"width=device-width, initial-scale=1\" name=\"viewport\"><meta content=\"Brad Griffin, Technology Professional Portfolio\" name=\"description\"><meta content=\"Brad Griffin\" name=\"author\"><meta name=\"keywords\" content = \"technology, professional, student, college, intern, professionals, thomas, thomas college\"><link href=\"images/logo.png\" rel=\"shortcut icon\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\"><link href=\"https://fonts.googleapis.com/css?family=Roboto+Slab:400,700\" rel=\"stylesheet\" type=\"text/css\"><link href=\"https://fonts.googleapis.com/css?family=Roboto:400,300,500,700\" rel=\"stylesheet\" type=\"text/css\"><link href=\"css/bootstrap.min.css\" rel=\"stylesheet\"><link href=\"css/owl.carousel.css\" rel=\"stylesheet\"><link href=\"css/bootstrap.min.css\" rel=\"stylesheet\"><link href=\"css/owl.carousel.css\" rel=\"stylesheet\"><link href=\"css/owl.theme.css\" rel=\"stylesheet\"><link href=\"css/magnific-popup.css\" rel=\"stylesheet\"><link href=\"css/style.css\" rel=\"stylesheet\"><link href=\"css/responsive.css\" rel=\"stylesheet\"><script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script><script type=\"text/javascript\" src=\"js/navigation.js\"></script>";
@@ -14,7 +10,11 @@ $(function(){
             $("#linkContent").html(linkContent);
       }
 
-
+      if( $('#splash-screen').length )
+      {
+            var splashScreen = "<section id=\"home\"><div class=\"container\"><div class=\"row\"><div class=\"wrap-hero-content\"><div class=\"hero-content\"><h1>Hello</h1><br><span class=\"typed\"></span></div></div><div class=\"mouse-icon margin-20\"><div class=\"scroll\"></div></div></div></div></section>";
+          $('#splash-screen').html(splashScreen);
+      }
 
       if($("#innerJavascript").length){
             var innerJavascript = "<script src=\"js/jquery-1.9.1.min.js\" type=\"text/javascript\"></script><script src=\"js/jquery.appear.js\" type=\"text/javascript\"></script><script src=\"js/bootstrap.min.js\" type=\"text/javascript\"></script><script src=\"js/classie.js\" type=\"text/javascript\"></script><script src=\"js/owl.carousel.min.js\" type=\"text/javascript\"></script><script src=\"js/jquery.magnific-popup.min.js\" type=\"text/javascript\"></script><script src=\"js/masonry.pkgd.min.js\" type=\"text/javascript\"></script><script src=\"js/masonry.js\" type=\"text/javascript\"></script><script src=\"js/smooth-scroll.min.js\" type=\"text/javascript\"></script><script src=\"js/typed.js\" type=\"text/javascript\"></script><script src=\"js/main.js\" type=\"text/javascript\"></script>";
