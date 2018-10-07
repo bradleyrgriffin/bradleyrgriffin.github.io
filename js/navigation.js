ce77d90601d5349d33a7f5763e922d06cd6087c6
Brad <brad.raymond.griffin@gmail.com>
2018-10-07 19:36:28 -0400

    Update js/navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -13,7 +13,7 @@ $(function(){
       if( $('#splash-screen').length )
       {
             var splashScreen = "<section id=\"home\"><div class=\"container\"><div class=\"row\"><div class=\"wrap-hero-content\"><div class=\"hero-content\"><h1>Hello</h1><br><span class=\"typed\"></span></div></div><div class=\"mouse-icon margin-20\"><div class=\"scroll\"></div></div></div></div></section>";
-          $('#splash-screen').html(splashScreen);
+          $("#splash-screen").html(splashScreen);
       }
 
       if($("#innerJavascript").length){
