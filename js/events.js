5ea54f8cce595468af00914d777be5f26fce2637
Brad <brad.raymond.griffin@gmail.com>
2018-12-17 16:12:57 -0500

    nav load

---
+++ js/events.js
@@ -0,0 +1,9 @@
+//Ensures correct loading and processing of javascript functionality.
+"use strict";
+
+//Onready
+$(function(){
+  //Load Navigation bar
+  $("#navigation-bar").load("../data/navigation.html");
+  $("#logo").html("<img src=\"../images/logo.png\" alt=\"logo\"></img>");
+});
