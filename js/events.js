0584955fc7106e59f73dce7371e848176e5ddb4d
Brad <brad.raymond.griffin@gmail.com>
2018-12-17 16:37:58 -0500

    footer

--- js/events.js
+++ js/events.js
@@ -6,6 +6,7 @@ $(function(){
   //Load Navigation bar
   $("#navigation-bar").load("../data/navigation.html");
   loadNotification("This notification pane is under development.","info");
+  $("#footer-content").load("../data/footer.html");
 });
 
 function loadNotification(text, ind, html){
@@ -14,19 +15,19 @@ function loadNotification(text, ind, html){
   }else{
     switch(ind){
       case "info":
-          $("#notification-body").html("<div class=\"alert alert-info\"><strong>Info!</strong>" + text + "</div>");
+          $("#notification-body").html("<div class=\"alert alert-info\"><strong>Info!</strong> " + text + "</div>");
           break;
       case "success":
-          $("#notification-body").html("<div class=\"alert alert-success\"><strong>Success!</strong>" + text + "</div>");
+          $("#notification-body").html("<div class=\"alert alert-success\"><strong>Success!</strong> " + text + "</div>");
           break;
       case "warning":
-          $("#notification-body").html("<div class=\"alert alert-warning\"><strong>Warning!</strong>" + text + "</div>");
+          $("#notification-body").html("<div class=\"alert alert-warning\"><strong>Warning!</strong> " + text + "</div>");
           break;
       case "danger":
-          $("#notification-body").html("<div class=\"alert alert-danger\"><strong>Error!</strong>" + text + "</div>");
+          $("#notification-body").html("<div class=\"alert alert-danger\"><strong>Error!</strong> " + text + "</div>");
           break;
       default:
-            $("#notification-body").html("<div class=\"alert alert-info\"><strong>Info!</strong>" + text + "</div>");
+            $("#notification-body").html("<div class=\"alert alert-info\"><strong>Info!</strong> " + text + "</div>");
             break;
     }
   }
