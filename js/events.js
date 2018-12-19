624451506437ee5f7eab4170b28b7acccf5ec5ed
Brad <brad.raymond.griffin@gmail.com>
2018-12-18 20:26:50 -0500

    fix

--- js/events.js
+++ js/events.js
@@ -8,7 +8,6 @@ $(function(){
   loadNotification("This notification pane is under development.","info");
   $("#footer-content").load("../data/footer.html");
 
-  addModal();
 });
 
 
@@ -38,12 +37,17 @@ function loadNotification(text, ind, html){
 
 function successDialog(){
     addModal("Success!", "Operation completed Successfully");
+    triggerModal();
 }
 
 function addModal(title, text, btnArray, footer){
 
   var itm = $("<!-- Modal --><div class=\"modal fade\" id=\"popUpWindow\" role=\"dialog\"><div class=\"modal-dialog\"><!-- Modal content--><div class=\"modal-content\"><div class=\"modal-header\"><button type=\"button\" class=\"close\" data-dismiss=\"modal\">&times;</button><h4 class=\"modal-title\">" + (title !== null && title !== undefined? title:"Operation") + "</h4></div><div class=\"modal-body\"><p>" + (text !== null && text !== undefined? text:"No Message Text Available.") + "</p></div><div class=\"modal-footer\"><button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button></div></div></div></div>");
 
+  if($('#popUpWindow')){
+    $('#popUpWindow').detach();
+  }
+  
   $('#container').append(itm);
 }
 
