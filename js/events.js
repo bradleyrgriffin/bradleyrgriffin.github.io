5154666bfd613e8eee86ce19b53aface0220ae93
Brad <brad.raymond.griffin@gmail.com>
2018-12-18 20:18:35 -0500

    fix

--- js/events.js
+++ js/events.js
@@ -36,13 +36,18 @@ function loadNotification(text, ind, html){
   }
 }
 
-function addModal(){
-  var itm = $("<!-- Modal --><div class=\"modal fade\" id=\"myModal\" role=\"dialog\"><div class=\"modal-dialog\"><!-- Modal content--><div class=\"modal-content\"><div class=\"modal-header\"><button type=\"button\" class=\"close\" data-dismiss=\"modal\">&times;</button><h4 class=\"modal-title\">Modal Header</h4></div><div class=\"modal-body\"><p>Some text in the modal.</p></div><div class=\"modal-footer\"><button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button></div></div></div></div>");
+function successDialog(){
+    addModal("Success!", "Operation completed Successfully");
+}
 
-  $('#container-content').append(itm);
+function addModal(title, text, btnArray, footer){
+
+  var itm = $("<!-- Modal --><div class=\"modal fade\" id=\"popUpWindow\" role=\"dialog\"><div class=\"modal-dialog\"><!-- Modal content--><div class=\"modal-content\"><div class=\"modal-header\"><button type=\"button\" class=\"close\" data-dismiss=\"modal\">&times;</button><h4 class=\"modal-title\">" + (title !== null && title !== undefined? title:"Operation") + "</h4></div><div class=\"modal-body\"><p>" + (text !== null && text !== undefined? text:"No Message Text Available.") + "</p></div><div class=\"modal-footer\"><button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button></div></div></div></div>");
+
+  $('#container').append(itm);
 }
 
 function triggerModal(){
-    $('#myModal').modal('show');
+    $('#popUpWindow').modal('show');
     //$('#myModel').modal('toggle');
 }
