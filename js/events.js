2409269459f14e403aee3a21e063561c6c01847e
Brad <brad.raymond.griffin@gmail.com>
2018-12-18 19:20:14 -0500

    model

--- js/events.js
+++ js/events.js
@@ -8,31 +8,26 @@ $(function(){
   loadNotification("This notification pane is under development.","info");
   $("#footer-content").load("../data/footer.html");
 
-  // Get the modal
-var modal = document.getElementById('myModal');
+  const buttons = document.querySelectorAll(`button[data-modal-trigger]`);
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  for(let button of buttons) {
+    modalEvent(button);
   }
-}
+
+  function modalEvent(button) {
+    button.addEventListener('click', () => {
+      const trigger = button.getAttribute('data-modal-trigger');
+      const modal = document.querySelector(`[data-modal=${trigger}]`);
+      const contentWrapper = modal.querySelector('.content-wrapper');
+      const close = modal.querySelector('.close');
+
+      close.addEventListener('click', () => modal.classList.remove('open'));
+      modal.addEventListener('click', () => modal.classList.remove('open'));
+      contentWrapper.addEventListener('click', (e) => e.stopPropagation());
+
+      modal.classList.toggle('open');
+    });
+  }
 
 });
 
