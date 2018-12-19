c53469543016ef2ee18a27614b55394cb588b16d
Brad <brad.raymond.griffin@gmail.com>
2018-12-18 19:49:57 -0500

    fix

--- js/events.js
+++ js/events.js
@@ -8,6 +8,7 @@ $(function(){
   loadNotification("This notification pane is under development.","info");
   $("#footer-content").load("../data/footer.html");
 
+  addModal();
 });
 
 
@@ -35,6 +36,11 @@ function loadNotification(text, ind, html){
   }
 }
 
+function addModal(){
+  var itm = $("<!-- Modal --><div class=\"modal fade\" id=\"myModal\" role=\"dialog\"><div class=\"modal-dialog\"><!-- Modal content--><div class=\"modal-content\"><div class=\"modal-header\"><button type=\"button\" class=\"close\" data-dismiss=\"modal\">&times;</button><h4 class=\"modal-title\">Modal Header</h4></div><div class=\"modal-body\"><p>Some text in the modal.</p></div><div class=\"modal-footer\"><button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button></div></div></div></div>");
+
+  $('#container-content').append(itm);
+}
 
 function triggerModal(){
     $('#myModel').modal('show');
