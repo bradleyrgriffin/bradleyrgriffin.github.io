8d55268b3c788734a1d1abb00b44b346ff7b5a22
Brad <brad.raymond.griffin@gmail.com>
2019-02-15 17:04:39 -0500

    Update events.js

--- js/events.js
+++ js/events.js
@@ -1,34 +1,35 @@
-events.custom = {} || {
-  baseURL : location.protocol + "//" + location.host,
-  renderTemplate : function(name){
-                    // If the named remote template is not yet loaded and compiled
-                    // as a named template, fetch it. In either case, return a promise
-                    // (already resolved, if the template has already been loaded)
-                    var deferred = $.Deferred();
-                    if ($.templates[name]) {
-                      deferred.resolve();
-                    } else {
-                      $.getScript(
-                        baseURL + "templates/" + name + ".js")
-                        .then(function() {
-                          if ($.templates[name]) {
-                            deferred.resolve();
-                          } else {
-                            alert("Script: \"" + name + ".js\" failed to load");
-                            deferred.reject();
-                          }
-                        });
-                    }
-                    return deferred.promise();
-                  },
-  renderMenu : function(data){
-                $.when(
-                  renderTemplate("people")
-                )
-                .done(function() {
-                  // Render once all templates for template composition are loaded
-                  var html = $.templates.menu.render(data);
-                  $("#sidebar-menu-wrapper").html(html);
-                });
+events = {custom :{
+    baseURL : location.protocol + "//" + location.host,
+    renderTemplate : function(name){
+                      // If the named remote template is not yet loaded and compiled
+                      // as a named template, fetch it. In either case, return a promise
+                      // (already resolved, if the template has already been loaded)
+                      var deferred = $.Deferred();
+                      if ($.templates[name]) {
+                        deferred.resolve();
+                      } else {
+                        $.getScript(
+                          baseURL + "templates/" + name + ".js")
+                          .then(function() {
+                            if ($.templates[name]) {
+                              deferred.resolve();
+                            } else {
+                              alert("Script: \"" + name + ".js\" failed to load");
+                              deferred.reject();
+                            }
+                          });
+                      }
+                      return deferred.promise();
+                    },
+    renderMenu : function(data){
+                  $.when(
+                    renderTemplate("people")
+                  )
+                  .done(function() {
+                    // Render once all templates for template composition are loaded
+                    var html = $.templates.menu.render(data);
+                    $("#sidebar-menu-wrapper").html(html);
+                  });
+    }
   }
 }
