e71d99d3d5869fcb32ca2fca30d5a0083674e8eb
Brad <brad.raymond.griffin@gmail.com>
2019-02-15 18:52:25 -0500

    change

--- js/events.js
+++ js/events.js
@@ -1,36 +1,5 @@
 events = {custom :{
     baseURL : location.protocol + "//" + location.host,
-    renderTemplate : function(name){
-                      // If the named remote template is not yet loaded and compiled
-                      // as a named template, fetch it. In either case, return a promise
-                      // (already resolved, if the template has already been loaded)
-                      var deferred = $.Deferred();
-                      if ($.templates[name]) {
-                        deferred.resolve();
-                      } else {
-                        $.getScript(
-                          events.custom.baseURL + "templates/" + name + ".js")
-                          .then(function() {
-                            if ($.templates[name]) {
-                              deferred.resolve();
-                            } else {
-                              alert("Script: \"" + name + ".js\" failed to load");
-                              deferred.reject();
-                            }
-                          });
-                      }
-                      return deferred.promise();
-                    },
-    renderMenu : function(data){
-                  $.when(
-                    events.custom.renderTemplate("menu")
-                  )
-                  .done(function() {
-                    // Render once all templates for template composition are loaded
-                    var html = $.templates.menu.render(data);
-                    $("#sidebar-menu-wrapper").html(html);
-                  });
-    },
     loadTemplate: function(tmpl, container, data, callback){
       var file = events.custom.baseURL + "/templates/" + tmpl + ".html";
       $.when($.get(file))
@@ -42,15 +11,38 @@ events = {custom :{
            }
        });
     }
+  },
+  data: {
+    menuItems : {menuItem:[{modId:1, modName:"About"},
+                           {modId:2, modName:"Education"},
+                           {modId:3, modName:"Experience"}
+                         ]},
+    form_01 : {
+       data:{sections: [
+                         {rows:[
+                                 {questions:[{name:"Email", type:"email"}, {name:"First Name:", type:"text"}], id:111},
+                                 {questions:[{name:"Email 2", type:"email"}, {name:"First Name 2:", type:"text"}], id:222}
+                               ],
+                         id: 111},
+                         {rows:[
+                                 {questions:[{name:"Email 3", type:"email"}, {name:"First Name 3:", type:"text"}], id:333},
+                                 {questions:[{name:"Email 4", type:"email"}, {name:"First Name 4:", type:"text"}], id:444}
+                               ],
+                         id: 222}
+                       ]},
+        id: 99999
+     }
   }
 }
 
 //OnLoad
 $(function(){
-  var data = {menuItem:[{modId:1, modName:"Module 1"},
-                        {modId:2, modName:"Module 2"},
-                        {modId:3, modName:"Module 3"}]}
-  events.custom.loadTemplate("menu", "#sidebar-menu-wrapper", data, function(){
-    window.alert("Haha!");
+  //Load Menu
+  events.custom.loadTemplate("menu", "#sidebar-menu-wrapper", events.data.menuItems, function(){
+    console.log("Menu Loaded");
+  });
+
+  events.custom.loadTemplate("form_01", "#page-content-wrapper", events.data.form_01, function(){
+    console.log("form_01 Loaded");
   });
 });
