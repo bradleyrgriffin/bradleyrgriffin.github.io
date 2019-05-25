62deb4cb11618647b4b02ea3345b1dacdacdb560
Brad <36484043+bradleyrgriffin@users.noreply.github.com>
2019-05-25 16:39:21 -0400

    s

--- js/properties.js
+++ js/properties.js
@@ -1,26 +1,15 @@
-// props = {modules:{
-//                         1: 'about',
-//                         2: 'education',
-//                         3: 'experience',
-//                         4: 'contact',
-//                         5: 'website-design'
-//                     },
-//             text:{
-//                   TXT_SUCCESS_MSG = 'Operation Completed Successfully!',
-//                   TXT_FAILURE_MSG = 'Operation Failed.',
-//                   TXT_SUBMIT_BTN = 'Submit',
-//                   TXT_SAVE_BTN = 'Save',
-//                   TXT_CANCEL_BTN = 'Cancel',
-//                   TXT_BTN_EXIT = 'Exit'
-//                   }
-// }
-
 props = {
   // This can have nested stuff, arrays, etc.
   modules: {1 : 'about',
             2 : 'education',
-            3 : 'contact'},
-  size: 'big',
-  enabled: true,
-  count: 5
+            3 : 'contact',
+            4 : 'website-design'},
+  text : {
+    TXT_SUCCESS_MSG : 'Operation Completed Successfully!',
+    TXT_FAILURE_MSG : 'Operation Failed.',
+    TXT_SUBMIT_BTN : 'Submit',
+    TXT_SAVE_BTN : 'Save',
+    TXT_CANCEL_BTN : 'Cancel',
+    TXT_BTN_EXIT : 'Exit'
+  }
 }
