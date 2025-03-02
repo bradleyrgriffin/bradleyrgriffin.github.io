d233a2a7e45a49cbfaa01636a97958af2210ca4c
bradleyrgriffin <brad.raymond.griffin@gmail.com>
2025-03-01 20:20:48 -0500

    fixing cypress assertion

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'https://bradleyrgriffin.me',
+    baseUrl: 'https://www.bradleyrgriffin.me',
     supportFile: 'cypress/support/index.js',
     setupNodeEvents(on, config) {
       // implement node event listeners here
